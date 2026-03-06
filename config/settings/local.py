05b3111406da4093deb032aa38c7641c427a60df
2joonkim <joon271694@gmail.com>
2026-03-06 16:44:36 +0900

    🐛 fix: swaager-ui 관련 설정파일 수정 (ui깨짐방지)

--- config/settings/local.py
+++ config/settings/local.py
@@ -9,7 +9,7 @@ if DEBUG:
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"

f8e60cbfd4b83c00403bee71b2793c6842038134
Justman-yzz <a01088744395@gmail.com>
2026-03-09 16:43:08 +0900

    🐛 fix      :ci에러 수정

--- apps/community/models/comment_model.py
+++ apps/community/models/comment_model.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-from apps.users.models import User
 from apps.core.models import TimeStampModel
+from apps.users.models import User
 
 
 class PostComment(TimeStampModel):

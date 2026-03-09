cc3195fb79dbf3b5bcb0468b5f3687a1dca89ee6
whdgk7176git config --global user.name whdgk7176 <whdgk7176@gmail.com>
2026-03-09 15:37:08 +0900

    🎨 style    : 코드 포매팅 수정

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -5,32 +5,14 @@ class Course(models.Model):
 
     id = models.BigAutoField(primary_key=True)
 
-    name = models.CharField(
-        max_length=30,
-        unique=True
-    )
+    name = models.CharField(max_length=30, unique=True)
 
-    tag = models.CharField(
-        max_length=3,
-        unique=True
-    )
+    tag = models.CharField(max_length=3, unique=True)
 
-    description = models.CharField(
-        max_length=255,
-        null=True,
-        blank=True
-    )
+    description = models.CharField(max_length=255, null=True, blank=True)
 
-    thumbnail_img_url = models.URLField(
-        max_length=255,
-        null=True,
-        blank=True
-    )
+    thumbnail_img_url = models.URLField(max_length=255, null=True, blank=True)
 
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
+    updated_at = models.DateTimeField(auto_now=True)

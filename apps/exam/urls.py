c61fd49a94d454c1a638f32093132b6ec99f5181
jun2k5 <jun2k5@naver.com>
2026-03-10 15:11:00 +0900

    ✨ feat     : exam 모델의 CRUD 기능 serializers 및 API View 작성, url 등록
    # Conflicts:
    #	config/urls.py

---
+++ apps/exam/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from apps.exam import views
+
+urlpatterns = [
+    path("admin/exams", views.ExamListCreateAPIView.as_view(), name="exam-list-create"),
+    path("admin/exams/<int:exam_id>", views.ExamDetailAPIView.as_view(), name="exam-detail"),
+]
+
+
--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns: list[URLPattern | URLResolver] = [
+    path("api/v1/", include("apps.exam.urls")),
     path("api/v1/posts/", include("apps.community.urls")),
     path("api/v1/accounts/", include("apps.users.urls.urls")),
     path("api/v1/", include("apps.exam.urls.urls")),

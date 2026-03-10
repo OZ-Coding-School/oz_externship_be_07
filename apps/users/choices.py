b0d10e4d1ca58901e98756b9c92962315a70702e
SUNO-Back <tnsgusdh1234@gmail.com>
2026-03-10 11:14:11 +0900

    💡 chore    : models.py와 choices.py 재설정 및 마이그레이션 파일 재생성, serializers,urls 파일 생성만

--- apps/users/choices.py
+++ apps/users/choices.py
@@ -1,28 +1,66 @@
 from django.db import models
 
 
-class UserRole(models.TextChoices):
-    USER = ("USER",)
-    STUDENT = ("STUDENT",)
-    TA = ("TA",)
-    OM = ("OM",)
-    ADMIN = ("ADMIN",)
-
-
+# 성별
 class UserGender(models.TextChoices):
-    MALE = ("MALE",)
-    FEMALE = "FEMALE"
+    MALE = "M"
+    FEMALE = "F"
 
 
+# 소셜 종류
 class SocialProvider(models.TextChoices):
     KAKAO = "kakao"
     NAVER = "naver"
 
 
+# 유저 권한
+class UserRole(models.TextChoices):
+    USER = "USER"
+    STUDENT = "STUDENT"
+    TA = "TA"
+    OM = "OM"
+    LC = "LC"
+    ADMIN = "ADMIN"
+
+
+# 유저 상태
+class UserStatus(models.TextChoices):
+    ACTIVATED = "ACTIVATED"
+    DEACTIVATED = "DEACTIVATED"
+    WITHDREW = "WITHDREW"
+
+
+# 수강 진행 상황
+class CohortStatus(models.TextChoices):
+    PENDING = "PENDING"
+    IN_PROGRESS = "IN_PROGRESS"
+    COMPLETED = "COMPLETED"
+
+
+# 수강신청 상태
+class EnrollmentStatus(models.TextChoices):
+    PENDING = "PENDING"
+    ACCEPTED = "ACCEPTED"
+    REJECTED = "REJECTED"
+    CANCELED = "CANCELED"
+
+
+# 분석 주기
+class AnalyticsInterval(models.TextChoices):
+    MONTHLY = "MONTHLY"
+    YEARLY = "YEARLY"
+
+
+# 탈퇴 사유
 class WithdrawalReason(models.TextChoices):
-    GRADUATION = "GRADUATION", "수강완료"
-    TRANSFER = "TRANSFER", "타 부트캠프에 더 양질의 컨텐츠가 있어서"
-    NO_LONGER_NEEDED = "NO_LONGER_NEEDED", "더 이상 필요가 없어서"
-    SERVICE_DISSATISFACTION = "SERVICE_DISSATISFACTION", "사이트내 UX/UI가 불편해서"
-    PRIVACY_CONCERN = "PRIVACY_CONCERN", "개인정보 우려 "
-    OTHER = "OTHER", "기타(직접입력)"
+    GRADUATION = "GRADUATION", "졸업"
+    TRANSFER = "TRANSFER", "전과/이직"
+    NO_LONGER_NEEDED = "NO_LONGER_NEEDED", "더 이상 필요하지 않음"
+    LACK_OF_INTEREST = "LACK_OF_INTEREST", "관심 부족"
+    TOO_DIFFICULT = "TOO_DIFFICULT", "수업이 너무 어려움"
+    FOUND_BETTER_SERVICE = "FOUND_BETTER_SERVICE", "더 나은 서비스 발견"
+    PRIVACY_CONCERNS = "PRIVACY_CONCERNS", "개인정보 보안 우려"
+    POOR_SERVICE_QUALITY = "POOR_SERVICE_QUALITY", "서비스 품질 불만족"
+    TECHNICAL_ISSUES = "TECHNICAL_ISSUES", "기술적 문제"
+    LACK_OF_CONTENT = "LACK_OF_CONTENT", "콘텐츠 부족"
+    OTHER = "OTHER", "기타"
--- apps/users/models.py
+++ apps/users/models.py
@@ -16,8 +16,7 @@ class User(TimeStampModel):
     birthday = models.DateField(verbose_name="생년월일")
     profile_img_url = models.CharField(max_length=255, null=True, blank=True)
     is_active = models.BooleanField(default=False)
-
-    role = models.CharField(max_length=10, choices=UserRole, default="User", verbose_name="역할")
+    role = models.CharField(max_length=10, choices=UserRole, default="User")
 
     class Meta:
         db_table = "user"

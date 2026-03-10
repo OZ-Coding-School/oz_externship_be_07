9d311633b222f7da1bfde4510e38de0367e59372
sangbo <tkdqh57@naver.com>
2026-03-10 14:33:50 +0900

    ♻️ refactor : QuestionList, QuestionDetail, QuestionCreate로 나누어서 작성,  author 작성, category_id를 받을 수 있게 수정

--- apps/questions/questions_serializers.py
+++ apps/questions/questions_serializers.py
@@ -1,13 +1,27 @@
 from rest_framework import serializers
 
+from apps.users.models import User
+
 from .models import QuestionCategories, QuestionImages, Questions
 
 
+class AuthorSerializer(serializers.ModelSerializer[User]):
+    class Meta:
+        model = User
+        fields = ["id", "nickname", "profile_img_url"]
+
+
 # 카테고리 시리얼라이저 (질문 등록시 선택)
-class QuestionCategorySerializer(serializers.ModelSerializer[QuestionCategories]):
+class CategorySerializer(serializers.ModelSerializer[QuestionCategories]):
+    depth = serializers.SerializerMethodField()
+    names = serializers.ReadOnlyField(source="name")
+
     class Meta:
         model = QuestionCategories
-        fields = ["id", "name", "parent"]
+        fields = ["id", "depth", "names"]
+
+    def get_depth(self, obj: QuestionCategories) -> int:
+        return 1 if obj.parent is None else 2
 
 
 # 질문 이미지 시리얼라이저
@@ -17,31 +31,34 @@ class QuestionImagesSerializer(serializers.ModelSerializer[QuestionImages]):
         fields = ["id", "img_url"]
 
 
-# 질문 등록 및 상세 조회(카테고리) 시리얼라이저
-class QuestionsSerializer(serializers.ModelSerializer[Questions]):
-    author_nickname = serializers.ReadOnlyField(source="author.nickname")
-    view_count = serializers.IntegerField(read_only=True)
-    images = QuestionImagesSerializer(many=True, read_only=True)
-    created_at = serializers.DateTimeField(read_only=True, format="%Y-%m-%d")
-    updated_at = serializers.DateTimeField(read_only=True, format="%Y-%m-%d")
-
-    # 카테고리 정보(입력할때는 id를 받지만, 보여줄때는 이름을 보여줄 수 있게 설정)
-    category_name = serializers.ReadOnlyField(source="category.name")
+# 목록 조회용: 가볍게
+class QuestionListSerializer(serializers.ModelSerializer[Questions]):
+    author = AuthorSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
 
     class Meta:
         model = Questions
-        fields = [
-            "id",
-            "author_nickname",
-            "category",
-            "category_name",
-            "title",
-            "content",
-            "view_count",
-            "images",
-            "created_at",
-            "updated_at",
-        ]
+        fields = ["id", "author", "category", "title", "content", "view_count", "created_at"]
+
+
+# 목록 조회용: 모든 정보가 나오게
+class QuestionDetailSerializer(serializers.ModelSerializer[Questions]):
+    author = AuthorSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
+    images = QuestionImagesSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Questions
+        fields = ["id", "author", "category", "images", "title", "content", "view_count", "created_at", "updated_at"]
+
+
+# 등록용: 입력받을 필드만 입력
+class QuestionCreateSerializer(serializers.ModelSerializer[Questions]):
+    images = QuestionImagesSerializer(required=False, many=True)
+
+    class Meta:
+        model = Questions
+        fields = ["category", "title", "content", "images"]
 
     # 글자 수 유효성 검사
     def validate_title(self, value: str) -> str:

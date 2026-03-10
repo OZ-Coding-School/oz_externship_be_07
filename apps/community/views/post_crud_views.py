42ad9432c318bbb8708e78df94e4b1e26619b308
PJG8806 <jinguy39@gmail.com>
2026-03-10 16:09:30 +0900

    🐛 fix       post_serializers_crud, post_views_crud 파일명 수정

--- apps/community/views/post_crud_views.py
+++ apps/community/views/post_crud_views.py
@@ -6,7 +6,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.community.serializers.post_serializers_crd import PostSerializer
+from apps.community.serializers.post_crud_serializers import PostSerializer
 
 
 class PostCreate(APIView):

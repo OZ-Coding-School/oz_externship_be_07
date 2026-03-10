d2f02a66d895b4af288232e7f1e48bda6f7147c0
SuyeongUeno <flrhsaks77@gmail.com>
2026-03-10 14:15:12 +0900

    ✨ feat     : 게시글 목록 조회 Spec-API 작성
    # Conflicts:
    #	apps/community/serializers/__init__.py
    #	apps/community/urls/__init__.py
    #	apps/community/views/__init__.py
    #	config/urls.py

--- apps/urls/post_url.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from apps.community.views import PostListCreateAPIView, PostDeleteAPIView
-
-urlpatterns = [
-    path("posts/", PostListCreateAPIView.as_view(), name="post-list-create"),
-    path("posts/<int:post_id>/", PostDeleteAPIView.as_view(), name="post-delete"),
-]

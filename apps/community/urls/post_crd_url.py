5c10d99d5284584c528a57b6245b0bc9381d2b85
PJG8806 <jinguy39@gmail.com>
2026-03-10 16:11:43 +0900

    🐛 fix       post 파일 명 오류 수정

--- apps/community/urls/post_crd_url.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from apps.community.views.post_crud_views import PostCreate, PostDetailUpdateDelete
-
-urlpatterns = [
-    path("", PostCreate.as_view(), name="post"),
-    path("/<int:post_id>", PostDetailUpdateDelete.as_view(), name="post"),
-]

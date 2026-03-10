f84b6b436708fe1067b44323e1908839394d3ed1
PJG8806 <jinguy39@gmail.com>
2026-03-10 16:54:14 +0900

    🐛 fix      :community urls 변경사항 수정

--- apps/community/urls/post_cud_url.py
+++ apps/community/urls/post_cud_url.py
@@ -3,6 +3,6 @@ from django.urls import path
 from apps.community.views.post_cud_views import PostCreate, PostDetailUpdateDelete
 
 urlpatterns = [
-    path("", PostCreate.as_view(), name="post"),
-    path("/<int:post_id>", PostDetailUpdateDelete.as_view(), name="post"),
+    path("", PostCreate.as_view(), name="post_create"),
+    path("/<int:post_id>", PostDetailUpdateDelete.as_view(), name="post_detail_update_delete"),
 ]

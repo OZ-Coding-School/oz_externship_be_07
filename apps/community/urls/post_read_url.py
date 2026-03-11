b5b4fb479824e402507fbdb185ae89c6eecca618
SuyeongUeno <flrhsaks77@gmail.com>
2026-03-11 11:17:08 +0900

    💡 chore    : post_read_url.py 수정

--- apps/community/urls/post_read_url.py
+++ apps/community/urls/post_read_url.py
@@ -4,6 +4,6 @@ from apps.community.views.post_detail_view import PostDetailAPIView
 from apps.community.views.post_list_view import PostListAPIView
 
 urlpatterns = [
-    path("posts/", PostListAPIView.as_view(), name="post-list"),
-    path("posts/<int:post_id>/", PostDetailAPIView.as_view(), name="post-detail"),
+    path("", PostListAPIView.as_view(), name="post-list"),
+    path("<int:post_id>/", PostDetailAPIView.as_view(), name="post-detail"),
 ]

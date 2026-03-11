05bb185c515650ceaf6a9e6674cd130b7e98ac8b
SuyeongUeno <flrhsaks77@gmail.com>
2026-03-11 17:43:00 +0900

    💡 chore    : ci 에러 수정

--- apps/community/serializers/__init__.py
+++ apps/community/serializers/__init__.py
@@ -1,5 +1,5 @@
-from .post_list_serializer import *
-from .post_detail_serializer import *
 from .category_serializer import PostCategoryListSpecSerializer, PostCategorySerializer
 from .comment_serializers import PostCommentSerializer
 from .post_cud_serializers import *
+from .post_detail_serializer import *
+from .post_list_serializer import *
--- apps/community/views/__init__.py
+++ apps/community/views/__init__.py
@@ -1,5 +1,5 @@
-from .post_list_view import *
-from .post_detail_view import *
 from .category_view import PostCategoryListSpecAPIView
 from .comment_views import CommentViewSet
 from .post_cud_views import PostCreate, PostDetailUpdateDelete
+from .post_detail_view import *
+from .post_list_view import *

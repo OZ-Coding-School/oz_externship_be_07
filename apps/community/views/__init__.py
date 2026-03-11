4003969e1dd9b3f3203b6dab34ed134e48236e39
SuyeongUeno <flrhsaks77@gmail.com>
2026-03-11 17:43:00 +0900

    💡 chore    : ci 에러 수정

--- apps/community/views/__init__.py
+++ apps/community/views/__init__.py
@@ -1,5 +1,3 @@
-from .post_list_view import *
-from .post_detail_view import *
 from .category_view import PostCategoryListSpecAPIView
 from .comment_views import CommentViewSet
 from .post_cud_views import PostCreate, PostDetailUpdateDelete

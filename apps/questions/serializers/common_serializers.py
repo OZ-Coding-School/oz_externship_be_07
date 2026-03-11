a680317ae4ce2099293b9da6debf15009da16bcb
sangbo <tkdqh57@naver.com>
2026-03-11 19:37:12 +0900

    🐛 fix      : black, isort, mypy진행 💡 chore    : serializer 폴더 구조 세분화 및 임포트 경로 오류 수정 ♻️ refactor : serializers(questions,answer) 코드 수정

--- apps/questions/serializers/common_serializers.py
+++ apps/questions/serializers/common_serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 from apps.users.models import User
 
 
-class AuthorSerializer(serializers.ModelSerializer):
-    profile_image_url = serializers.ReadOnlyField(source='profile_img_url')
+class AuthorSerializer(serializers.ModelSerializer[User]):
+    profile_image_url = serializers.ReadOnlyField(source="profile_img_url")
     course_name = serializers.CharField(read_only=True, default=None)
     cohort_name = serializers.IntegerField(read_only=True, default=1)
 
--- apps/questions/serializers/questions_answer_serializers.py
+++ apps/questions/serializers/questions_answer_serializers.py
@@ -1,33 +1,31 @@
 from rest_framework import serializers
 
+from ..models import AnswerComments, AnswerImages, Answers
 from .common_serializers import AuthorSerializer
-from ..models import AnswerImages, AnswerComments, Answers
 
 
-class AnswerImagesSerializer(serializers.ModelSerializer):
+class AnswerImagesSerializer(serializers.ModelSerializer[AnswerImages]):
     class Meta:
         model = AnswerImages
         fields = ["id", "img_url"]
 
-class AnswerCommentSerializer(serializers.ModelSerializer):
+
+class AnswerCommentSerializer(serializers.ModelSerializer[AnswerComments]):
     author = AuthorSerializer(read_only=True)
+
     class Meta:
         model = AnswerComments
         fields = ["id", "author", "content", "created_at"]
 
-class AnswersSerializer(serializers.ModelSerializer):
+
+class AnswersSerializer(serializers.ModelSerializer[Answers]):
     author = AuthorSerializer(read_only=True)
     comments = AnswerCommentSerializer(many=True, read_only=True)
     images = AnswerImagesSerializer(many=True, read_only=True)
 
-    image_urls = serializers.ListField(
-        child=serializers.URLField(), write_only=True, required=False
-    )
+    image_urls = serializers.ListField(child=serializers.URLField(), write_only=True, required=False)
 
     class Meta:
         model = Answers
-        fields = [
-            "id", "author", "content", "is_adopted",
-            "images", "image_urls", "comments", "created_at"
-        ]
+        fields = ["id", "author", "content", "is_adopted", "images", "image_urls", "comments", "created_at"]
         read_only_fields = ["is_adopted"]

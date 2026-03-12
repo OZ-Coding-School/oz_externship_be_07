44c41ae986cc54fb2eefed867f4cceabf5b016f2
sangbo <tkdqh57@naver.com>
2026-03-12 10:58:30 +0900

    ♻️ refactor : 코드 리뷰 부분 수정

--- apps/questions/serializers/common_serializers.py
+++ apps/questions/serializers/common_serializers.py
@@ -6,7 +6,7 @@ from apps.users.models import User
 class AuthorSerializer(serializers.ModelSerializer[User]):
     profile_image_url = serializers.ReadOnlyField(source="profile_img_url")
     course_name = serializers.CharField(read_only=True, default=None)
-    cohort_name = serializers.IntegerField(read_only=True, default=1)
+    cohort_name = serializers.CharField(read_only=True)
 
     class Meta:
         model = User

7e65a23a6fbdb64c0071b7d71b254340d0b99fd2
SUNO-Back <tnsgusdh1234@gmail.com>
2026-03-12 10:36:15 +0900

    💡 chore    : 파일 구조 변경

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import SignUpView
+from apps.users.views.signup_views import SignUpView
 
 urlpatterns = [
     path("signup", SignUpView.as_view(), name="signup"),

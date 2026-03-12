94bc50c1725a1396d3a6ed373ba4e949f1a6ea9c
Brandon Hyun <>
2026-03-12 18:00:27 +0900

    💡 chore: conflict resolved

--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,7 @@ urlpatterns: list[URLPattern | URLResolver] = [
     path("api/v1/posts/", include("apps.community.urls")),
     path("api/v1/accounts/", include("apps.users.urls.urls")),
     path("api/v1/admin/", include("apps.users.urls.admin_urls")),
+    path("api/v1/chatbot/", include("apps.chatbot.urls")),
 ]
 
 if settings.DEBUG:

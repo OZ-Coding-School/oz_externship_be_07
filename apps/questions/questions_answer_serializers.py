e3a83ed5ad2b1327e4bab0a6ca47b7791b0c5b72
gyugyu99 <marsins90@gmail.com>
2026-03-12 16:34:28 +0900

    🐛 fix      :resolve merge conflict

---
+++ apps/questions/questions_answer_serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import Answers, AnswerImages, AnswerComments
+from .questions_serializers import AuthorSerializer
+
+class AnswerImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AnswerImages
+        fields = ["id", "img_url"]
+
+class AnswerCommentSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+    class Meta:
+        model = AnswerComments
+        fields = ["id", "author", "content", "created_at"]
+
+class AnswersSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+    comments = AnswerCommentSerializer(many=True, read_only=True)
+    images = AnswerImagesSerializer(many=True, read_only=True)
+
+    image_urls = serializers.ListField(
+        child=serializers.URLField(), write_only=True, required=False
+    )
+
+    class Meta:
+        model = Answers
+        fields = [
+            "id", "author", "content", "is_adopted",
+            "images", "image_urls", "comments", "created_at"
+        ]
+        read_only_fields = ["is_adopted"]

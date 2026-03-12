5d6a0dda1fc14d59053601acb4ba277e4751ef8f
PJG8806 <jinguy39@gmail.com>
2026-03-12 13:30:56 +0900

    🐛 fix      :post test코드 mypy 오류 수정

--- apps/community/tests/views/test_post_cud_view.py
+++ apps/community/tests/views/test_post_cud_view.py
@@ -4,7 +4,7 @@ from rest_framework import status
 
 from apps.community.models.category_model import PostCategory
 from apps.community.models.post_model import Post
-from apps.users.models import User
+from apps.users.models.models import User
 
 
 class PostCreateUpdateDeleteViewTest(TestCase):

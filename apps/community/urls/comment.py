4c5875b7ccd80cf7cff5e40a255ec0b0a60f3843
HHJina <jina199234@gmail.com>
2026-03-12 19:08:29 +0900

    🐛 fix      : review 관련 수정

--- apps/community/urls/comment.py
+++ apps/community/urls/comment.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 from apps.community.views.comment_views import CommentViewSet
 
 urlpatterns = [
-    path("", CommentViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path("/<int:comment_id>", CommentViewSet.as_view({ 'put': 'update', 'delete': 'destroy'})),
+    path("", CommentViewSet.as_view({"get": "list", "post": "create"})),
+    path("/<int:comment_id>", CommentViewSet.as_view({"put": "update", "delete": "destroy"})),
 ]

60c0e38d94d75389eb6c19a4c69d7772685f1e21
2joonkim <joon271694@gmail.com>
2026-03-13 16:39:57 +0900

    🐛 fix: user/sign_up 409관련 에러 테스트코드 수정

--- apps/users/views/signup_views.py
+++ apps/users/views/signup_views.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from drf_spectacular.utils import OpenApiExample, OpenApiResponse, extend_schema
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
@@ -67,11 +68,10 @@ class SignUpView(APIView):
             user_service.create_user(serializer.validated_data)
             return Response({"detail": "회원가입이 완료되었습니다."}, status=status.HTTP_201_CREATED)
 
-        except ValidationError as e:
-            return Response({"error_detail": e.detail}, status=status.HTTP_400_BAD_REQUEST)
-
-        # 중복 가입 에러 처리
-        except Exception:
+        except IntegrityError:
             return Response(
                 {"error_detail": "이미 중복된 회원가입 내역이 존재합니다."}, status=status.HTTP_409_CONFLICT
             )
+
+        except ValidationError as e:
+            return Response({"error_detail": e.detail}, status=status.HTTP_400_BAD_REQUEST)

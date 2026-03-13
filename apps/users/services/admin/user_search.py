d97e8758a15754264ca7c19147ba88a8482a7015
wooryun <rhrjs4@naver.com>
2026-03-13 16:10:51 +0900

    💡 chore    : services 코드 수정 및 CI 테스트 검증

--- apps/users/services/admin/user_search.py
+++ apps/users/services/admin/user_search.py
@@ -1,8 +1,9 @@
 from apps.subject.models import CohortStudent
 from apps.users.models.models import User
+from typing import Any
 
 
-def get_user_course_data(user: User) -> dict | None:
+def get_user_course_data(user: User) -> dict[str, Any] | None:
     cohort_student = CohortStudent.objects.filter(user=user).select_related("cohort__course").last()
 
     if not cohort_student:

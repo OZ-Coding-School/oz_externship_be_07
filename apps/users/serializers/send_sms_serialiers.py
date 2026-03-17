988f2a8b625c8cd2100f68d9a0af1dfeb4b286c5
SUNO-Back <tnsgusdh1234@gmail.com>
2026-03-17 14:15:31 +0900

    ✨ feat     : 발급받은 SMS인증번호를 검증하는 기능구현

--- apps/users/serializers/send_sms_serialiers.py
+++ apps/users/serializers/send_sms_serialiers.py
@@ -7,7 +7,7 @@ from rest_framework import serializers
 class SmsSendSerializer(serializers.Serializer[Any]):
     phone_number = serializers.CharField(
         required=True,
-        validators=[RegexValidator(regex=r"^010-\d{4}-\d{4}$", message="올바른 휴대폰 번호 형식이 아닙니다.")],
+        validators=[RegexValidator(regex=r"^010-?\d{4}-?\d{4}$", message="올바른 휴대폰 번호 형식이 아닙니다.")],
     )
 
     def validate_phone_number(self, value: str) -> str:

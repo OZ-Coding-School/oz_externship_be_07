ae45cf7f9b4748bf599f19f0811c8eac151c8f08
jun2k5 <jun2k5@naver.com>
2026-03-17 14:14:51 +0900

    🎨 style    : 코드 포매팅 수정

--- apps/users/tests/test_user_enrollment.py
+++ apps/users/tests/test_user_enrollment.py
@@ -10,7 +10,6 @@ from rest_framework.test import APIClient
 from apps.subject.models.cohort_models import Cohort
 from apps.subject.models.course_models import Course
 from apps.subject.models.enrollment_request_models import EnrollmentRequest
-
 from apps.users.models.models import User
 
 

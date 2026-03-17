a690e3f24be472587292bae9dacc276cbf2fe4fb
wooryun <rhrjs4@naver.com>
2026-03-17 18:46:39 +0900

    💡 chore    : services 타임 스탬프 기능 수정

--- apps/users/services/admin/user_enrollment_accept.py
+++ apps/users/services/admin/user_enrollment_accept.py
@@ -1,10 +1,14 @@
+from django.utils import timezone
+
 from apps.subject.models.enrollment_request_models import EnrollmentRequest
 
 
 def accept_enrollment_requests(enrollment_ids: list[int]) -> int:
     """
-    대기 중인 수강 신청 건들을 승인 상태로 변경하고 변경된 건수를 반환합니다.
+    대기 중인 수강 신청 건들을 승인 상태로 변경하고 승인 시간을 기록합니다.
     """
-    updated_count = EnrollmentRequest.objects.filter(id__in=enrollment_ids, status="PENDING").update(status="ACCEPTED")
+    updated_count = EnrollmentRequest.objects.filter(id__in=enrollment_ids, status="PENDING").update(
+        status="ACCEPTED", accepted_at=timezone.now
+    )
 
     return updated_count

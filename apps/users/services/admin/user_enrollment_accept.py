5243ebb55ec824ccd4de8684835d9e967d0bd0ca
wooryun <rhrjs4@naver.com>
2026-03-18 10:31:54 +0900

    💡 chore    : test코드 오류수정

--- apps/users/services/admin/user_enrollment_accept.py
+++ apps/users/services/admin/user_enrollment_accept.py
@@ -8,7 +8,7 @@ def accept_enrollment_requests(enrollment_ids: list[int]) -> int:
     대기 중인 수강 신청 건들을 승인 상태로 변경하고 승인 시간을 기록합니다.
     """
     updated_count = EnrollmentRequest.objects.filter(id__in=enrollment_ids, status="PENDING").update(
-        status="ACCEPTED", accepted_at=timezone.now
+        status="ACCEPTED", accepted_at=timezone.now()
     )
 
     return updated_count

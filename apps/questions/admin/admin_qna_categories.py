fd58c3a0d719b8936cd968dd9d7693e80ce35995
pcw8233 <pcw3511@gmail.com>
2026-03-18 18:37:48 +0900

    ✨ feat     : admin_qna_categories API 작성 🐛 fix      : mypy,isort,black 최신화 ✨ feat: admin_qna_categories admin_qna_categoryserializers 작성
    test code 정상 실행

---
+++ apps/questions/admin/admin_qna_categories.py
@@ -0,0 +1,47 @@
+from rest_framework import permissions, status
+from rest_framework.request import Request  # Request 타입 추가
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from apps.questions.models import QuestionCategories
+from apps.questions.serializers.admin_qna_categoryserializers import (
+    AdminCategoryCreateSerializer,
+)
+
+
+# 어드민 카테고리 등록 API 기능 구현
+class AdminCategoryCreateAPIView(APIView):
+    permission_classes = [permissions.IsAdminUser]
+
+    def post(self, request: Request) -> Response:
+        category_type = request.data.get("category_type")
+        name = request.data.get("name")
+
+        if not category_type or not name:
+            return Response(
+                {"error_detail": "카테고리 종류와 이름은 필수 입력값입니다."}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        parent_id = request.data.get("parent_id")
+        if parent_id and not QuestionCategories.objects.filter(id=parent_id).exists():
+            return Response({"error_detail": "부모 카테고리를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+        # 중복 체크 및 데이터 저장
+        serializer = AdminCategoryCreateSerializer(data=request.data)
+
+        try:
+            if serializer.is_valid(raise_exception=True):
+                # 카테고리 생성 성공
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            # 시리얼라이저의 ValidationError 메시지와 동기화
+            if "이미 존재합니다" in str(e):
+                return Response(
+                    {"error_detail": "동일한 이름의 카테고리가 이미 존재합니다."}, status=status.HTTP_409_CONFLICT
+                )
+
+            return Response({"error_detail": "유효하지 않은 요청입니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({"error_detail": "알 수 없는 에러가 발생했습니다."}, status=status.HTTP_400_BAD_REQUEST)

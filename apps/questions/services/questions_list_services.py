efdfa3d4ed5ddfda4a89a37978cdb25054bd201f
sangbo <tkdqh57@naver.com>
2026-03-18 17:22:20 +0900

    ♻️ refactor : test pr coverage 올리기 위한 코드 수정

--- apps/questions/services/questions_list_services.py
+++ apps/questions/services/questions_list_services.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
-from django.db.models import QuerySet
+from django.db.models import F, Q, QuerySet
 from django.shortcuts import get_object_or_404
 
-from apps.questions.models import Questions
+from apps.questions.models import QuestionCategories, Questions
 
 
 # 질문 조회
@@ -13,7 +13,10 @@ class QuestionListService:
         queryset = Questions.objects.all()
 
         if category_id:
-            queryset = queryset.filter(category_id=category_id)
+            category_ids = QuestionCategories.objects.filter(
+                Q(id=category_id) | Q(parent_id=category_id) | Q(parent__parent_id=category_id)
+            ).values_list("id", flat=True)
+            queryset = queryset.filter(category_id__in=category_ids)
 
         if search_keyword:
             queryset = queryset.filter(title__contains=str(search_keyword))
@@ -23,9 +26,11 @@ class QuestionListService:
     # 질문 상세 조회
     @staticmethod
     def get_question_detail(question_id: int) -> Questions:
-        question = get_object_or_404(Questions, id=question_id)
+        queryset = Questions.objects.select_related("category", "author").prefetch_related("images", "answers")
 
-        question.view_count = question.view_count + 1
-        question.save()
+        question = get_object_or_404(queryset, id=question_id)
+
+        Questions.objects.filter(id=question_id).update(view_count=F("view_count") + 1)
+        question.refresh_from_db()
 
         return question

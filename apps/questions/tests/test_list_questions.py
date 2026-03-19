cffab55e52d2206c3cd78633d5b9858976e22fff
sangbo <tkdqh57@naver.com>
2026-03-19 13:49:29 +0900

    ♻️ refactor : APIClient 함수 이름 통일

--- apps/questions/tests/test_list_questions.py
+++ apps/questions/tests/test_list_questions.py
@@ -2,7 +2,6 @@ from django.db.models import QuerySet
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.test import APIClient
 
 from apps.questions.models import QuestionCategories, Questions
@@ -45,12 +44,10 @@ class QuestionListTest(TestCase):
 
     def test_get_questions_invalid_category_id(self) -> None:
         url = reverse("questions:question_list_create")
-        response: Response = self.api_client.get(url, {"category_id": "abc"})
+        response = self.client.get(url, {"category_id": "abc"})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data["error_detail"], "유효하지 않은 카테고리 ID입니다.")
 
     def test_get_question_detail_not_found(self) -> None:
         url = reverse("questions:question_detail", kwargs={"question_id": 99999})
-        response: Response = self.api_client.get(url)
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        self.assertEqual(response.data["error_detail"], "존재하지 않는 질문입니다.")

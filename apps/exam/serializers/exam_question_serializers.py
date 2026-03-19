e92db5b19611378f13dc62f39608e32a2901fb5a
whdgk7176git config --global user.name whdgk7176 <whdgk7176@gmail.com>
2026-03-19 14:59:50 +0900

    🎨 style    : 코드 포매팅 수정

--- apps/exam/serializers/exam_question_serializers.py
+++ apps/exam/serializers/exam_question_serializers.py
@@ -1,12 +1,13 @@
 import json
+from typing import any, dict
 
-from typing import Any, Dict
 from rest_framework import serializers
+
 from apps.exam.models.choices import QuestionType
 from apps.exam.models.exam_question_models import ExamQuestion
 
 
-class ExamQuestionCreateSerializer(serializers.Serializer[Dict[str, Any]]):
+class ExamQuestionCreateSerializer(serializers.Serializer[dict[str, any]]):
     type = serializers.ChoiceField(choices=QuestionType.choices)
     question = serializers.CharField(max_length=255)
     prompt = serializers.CharField(required=False, allow_null=True, allow_blank=True)
@@ -25,14 +26,14 @@ class ExamQuestionUpdateSerializer(ExamQuestionCreateSerializer):
     pass
 
 
-class ExamQuestionResponseSerializer(serializers.ModelSerializer[Dict[str, Any]]):
+class ExamQuestionResponseSerializer(serializers.ModelSerializer[dict[str, any]]):
     question_id = serializers.IntegerField(source="id")
     correct_answer = serializers.JSONField(source="answer")
     options = serializers.SerializerMethodField()
 
     class Meta:
         model = ExamQuestion
-        fields =  [
+        fields = [
             "question_id",
             "type",
             "question",
@@ -44,7 +45,7 @@ class ExamQuestionResponseSerializer(serializers.ModelSerializer[Dict[str, Any]]
             "explanation",
         ]
 
-    def get_options(self, obj: ExamQuestion) -> Any:
+    def get_options(self, obj: ExamQuestion) -> any:
         if not obj.options_json:
             return None
         try:
@@ -52,9 +53,11 @@ class ExamQuestionResponseSerializer(serializers.ModelSerializer[Dict[str, Any]]
         except (ValueError, TypeError, json.JSONDecodeError):
             return None
 
-class ExamQuestionDeleteResponseSerializer(serializers.Serializer[Dict[str, Any]]):
+
+class ExamQuestionDeleteResponseSerializer(serializers.Serializer[dict[str, any]]):
     exam_id = serializers.IntegerField()
     question_id = serializers.IntegerField()
 
-class ErrorDetailSerializer(serializers.Serializer[Dict[str, str]]):
+
+class ErrorDetailSerializer(serializers.Serializer[dict[str, str]]):
     error_detail = serializers.CharField()
--- apps/exam/urls/urls.py
+++ apps/exam/urls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from apps.exam.views.exam_question_views import (
     ExamQuestionDetailAPIView,
-    ExamQuestionListCreateAPIView
+    ExamQuestionListCreateAPIView,
 )
 
 app_name = "exam"
@@ -10,5 +10,4 @@ app_name = "exam"
 urlpatterns = [
     path("<int:exam_id>/questions", ExamQuestionListCreateAPIView.as_view(), name="exam-question-list-create"),
     path("questions/<int:question_id>", ExamQuestionDetailAPIView.as_view(), name="exam_question-detail"),
-
 ]

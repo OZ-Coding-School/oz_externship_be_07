997c5618cf2b9a0b9adafb40ff1034cbf74c6268
whdgk7176git config --global user.name whdgk7176 <whdgk7176@gmail.com>
2026-03-20 14:52:30 +0900

    ♻️ refactor : 리팩터링 (exams->exam)

--- apps/exams/migrations/0004_rename_subject_id_exam_subject_and_more.py
+++ apps/exams/migrations/0004_rename_subject_id_exam_subject_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("exams", "0003_alter_examdeployment_status_alter_examquestion_type"),
+        ("exam", "0003_alter_examdeployment_status_alter_examquestion_type"),
     ]
 
     operations = [

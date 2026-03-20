a4207d7eff6e91891b89ec547f6e2e639f30f76c
pcw8233 <pcw3511@gmail.com>
2026-03-20 18:17:47 +0900

    Commit Convention ♻️ refactor : urls 수정 관련 이슈: #123
    # Conflicts:
    #	config/urls.py

--- apps/questions/urls/qna_urls.py
+++ apps/questions/urls/qna_urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views.answers_views import AIAnswerViewSet, AnswerViewSet
-from .views.presigned_url_views import AnswerPresignedUrlView, QuestionPresignedUrlView
-from .views.questions_list_views import QuestionListDetailView, QuestionListView
+from apps.questions.views.answers_views import AIAnswerViewSet, AnswerViewSet
+from apps.questions.views.questions_list_views import (
+    QuestionListDetailView,
+    QuestionListView,
+)
 
 app_name = "questions"
 
@@ -40,17 +42,5 @@ urlpatterns = [
         "answers/<int:pk>/comments",
         AnswerViewSet.as_view({"post": "comment"}),
         name="answer_comment_create",
-    ),
-    # 답변 이미지 Presigned URL 발급
-    path(
-        "answers/presigned-url",
-        AnswerPresignedUrlView.as_view(),
-        name="answer_presigned_url",
-    ),
-    # 질문 이미지 Presigned URL 발급
-    path(
-        "questions/presigned-url",
-        QuestionPresignedUrlView.as_view(),
-        name="question_presigned_url",
     ),
 ]

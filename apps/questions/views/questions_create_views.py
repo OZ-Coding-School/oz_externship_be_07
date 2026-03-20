ae512175d727487aab238db03b7c21f64f5e3c73
sangbo <tkdqh57@naver.com>
2026-03-20 13:38:47 +0900

    🐛 fix      : black, isort, mypy 실행

--- apps/questions/views/questions_create_views.py
+++ apps/questions/views/questions_create_views.py
@@ -61,4 +61,7 @@ class QuestionCreateView(APIView):
             return Response({"error_detail": str(e)}, status=status.HTTP_403_FORBIDDEN)
         # 예상치 못한 서버 에러
         except Exception:
-            return Response({"error_detail": "서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(
+                {"error_detail": "서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )

0a8e1e687c5a57c07af26d0500329bd9c3015ffe
HHJina <jina199234@gmail.com>
2026-03-23 17:19:34 +0900

    🎨 style    : ci 수정

--- apps/community/tasks.py
+++ apps/community/tasks.py
@@ -1,7 +1,7 @@
-from celery import shared_task
+from celery import shared_task  # type: ignore
 from django.core.management import call_command
 
 
-@shared_task
-def sync_user_search_task():
+@shared_task  # type: ignore
+def sync_user_search_task() -> None:
     call_command("sync_user_search")

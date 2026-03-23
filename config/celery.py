70202643192520001a43ea3962ecbbda08735482
HHJina <jina199234@gmail.com>
2026-03-23 17:19:34 +0900

    🎨 style    : ci 수정

--- config/celery.py
+++ config/celery.py
@@ -1,7 +1,7 @@
 import os
 
-from celery import Celery
-from celery.schedules import crontab
+from celery import Celery  # type: ignore
+from celery.schedules import crontab  # type: ignore
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
 

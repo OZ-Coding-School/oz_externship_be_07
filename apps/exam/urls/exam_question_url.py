42cfca4ab62cce6a41a8ef7f8ffd2b9254a44df7
whdgk7176git config --global user.name whdgk7176 <whdgk7176@gmail.com>
2026-03-24 16:56:42 +0900

    ✨ feat     : exam_question view service 개선

---
+++ apps/exam/urls/exam_question_url.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from apps.exam.views.exam_question_views import (
+    ExamQuestionCreateAPIView,
+    ExamQuestionUpdateDeleteAPIView,
+)
+
+urlpatterns = [
+    path(
+        "<int:exam_id>/questions",
+        ExamQuestionCreateAPIView.as_view(),
+        name="exam-question-create",
+    ),
+    path(
+        "questions/<int:question_id>",
+        ExamQuestionUpdateDeleteAPIView.as_view(),
+        name="exam-question-update-delete",
+    ),
+]

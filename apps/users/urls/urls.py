2a7cc931f1f68e42564d20bf1f08f6b29520a8b9
wooryun <rhrjs4@naver.com>
2026-03-24 14:02:23 +0900

    💡 chore    : 코드 꼬임 해결

--- apps/users/urls/urls.py
+++ apps/users/urls/urls.py
@@ -9,6 +9,12 @@ from apps.users.views.re_token_views import ReTokenView
 from apps.users.views.send_email_views import EmailSendView
 from apps.users.views.send_sms_views import SmsSendView
 from apps.users.views.signup_views import SignUpView
+from apps.users.views.social_login_views import (
+    KakaoLoginCallbackView,
+    KakaoLoginStartView,
+    NaverLoginCallbackView,
+    NaverLoginStartView,
+)
 from apps.users.views.verify_email_views import EmailVerifyView
 from apps.users.views.verify_sms_views import SmsVerifyView
 

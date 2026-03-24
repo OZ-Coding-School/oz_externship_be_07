6c261b1a589e5a643f52d6f6bb39dd10f98305a1
PJG8806 <jinguy39@gmail.com>
2026-03-24 18:59:03 +0900

    🐛 fix       상수 community/core/constants.py로 모으기 수정

--- apps/community/core/constants.py
+++ apps/community/core/constants.py
@@ -1,2 +1,13 @@
+import re
+
 POST_VIEW_TTL = 60 * 60  # 1시간
 POST_CONTENT_PREVIEW_LENGTH = 50
+
+# 마크다운 이미지/링크 정규식
+RE_MARKDOWN_LINK = re.compile(r"(!?)\[(.*?)\]\((https?://[^\s\)]+)\)")
+RE_IMAGE_URL = re.compile(r"!\[.*?\]\((https?://[^?)\s]+)(?:\?.*?)?\)")
+RE_ATTACHMENT_URL = re.compile(r"(?<!\!)\[(.*?)\]\((https?://[^?)\s]+)(?:\?.*?)?\)")
+RE_FILE_URL_STRIP_QS = re.compile(r"(!?)\[(.*?)\]\((https?://[^?)\s]+)(?:\?.*?)?\)")
+
+# url 절취선 기준
+RIST_SPLIT = "com/"

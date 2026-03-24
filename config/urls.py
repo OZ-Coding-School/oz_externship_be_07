4034d190f71ac1a14a360fc8ac300fefe7d63fd4
jun2k5 <jun2k5@naver.com>
2026-03-24 14:27:07 +0900

    💡 chore    : config의 urls 오류 수정

--- config/urls.py
+++ config/urls.py
@@ -10,10 +10,10 @@ from drf_spectacular.views import (
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path("admin/", admin.site.urls),
-    path("api/v1/posts/", include("apps.community.urls")),
+    path("api/v1/posts", include("apps.community.urls")),
     path("api/v1/accounts/", include("apps.users.urls.urls")),
     path("api/v1/admin/", include("apps.users.urls.admin_urls")),
-    path("api/v1/qna/", include("apps.questions.qna_urls")),
+    path("api/v1/qna/", include("apps.questions.urls.qna_urls")),
     path("api/v1/", include("apps.subject.urls.cohort_urls")),
     path("api/v1/", include("apps.subject.urls.urls")),
     path("api/v1/", include("apps.exam.urls.exam_deployment_urls")),

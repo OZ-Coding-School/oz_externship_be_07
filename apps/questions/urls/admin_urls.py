35fe8e69c6f8ad52f431f09ca71119473842924e
pcw8233 <pcw3511@gmail.com>
2026-03-25 14:06:38 +0900

    ♻️ refactor : isort, black, mypy 최신화

--- apps/questions/urls/admin_urls.py
+++ apps/questions/urls/admin_urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from apps.questions.views.admin.admin_qna_categories_list_view import (
     AdminCategoryAPIView,
 )
-from apps.questions.views.admin.admin_qna_categories_views import (
-    AdminCategoryCreateAPIView,
-)
 
 app_name = "admin_questions"
 
--- apps/questions/urls/qna_urls.py
+++ apps/questions/urls/qna_urls.py
@@ -10,11 +10,6 @@ from apps.questions.views.questions_list_views import (
     QuestionListView,
 )
 from apps.questions.views.user_categories_list_views import UserCategoryListView
-from apps.questions.views.answers_views import AIAnswerViewSet, AnswerViewSet
-from apps.questions.views.questions_list_views import (
-    QuestionListDetailView,
-    QuestionListView,
-)
 
 app_name = "questions"
 

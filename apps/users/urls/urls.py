408a1028272e292f45fa18f98c1f896f1baa1605
SUNO-Back <tnsgusdh1234@gmail.com>
2026-03-25 15:05:20 +0900

    ✨ feat     : 내 수강목록 조회 기능 구현

--- apps/users/urls/urls.py
+++ apps/users/urls/urls.py
@@ -6,6 +6,7 @@ from apps.users.views.enroll_student import EnrollStudentAPIView
 from apps.users.views.find_email_views import FindEmailView
 from apps.users.views.login_views import LoginView
 from apps.users.views.logout_views import LogoutView
+from apps.users.views.mycouerse_check_views import MyEnrolledCourseView
 from apps.users.views.profile_views import (
     NicknameCheckView,
     ProfileImageView,
@@ -46,4 +47,5 @@ urlpatterns = [
     path("check-nickname", NicknameCheckView.as_view(), name="check-nickname"),
     path("me/profile-image", ProfileImageView.as_view(), name="profile-image"),
     path("change-phone", ChangePhoneNumberView.as_view(), name="change-phone"),
+    path("me/enrolled-courses", MyEnrolledCourseView.as_view(), name="me-enrolled-courses"),
 ]
---
+++ apps/users/views/mycouerse_check_views.py
@@ -0,0 +1,36 @@
+from typing import cast
+
+from apps.users.models.models import User
+from drf_spectacular.utils import OpenApiExample, extend_schema
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from apps.users.serializers.mycouerse_check_serializers import (
+    MyEnrolledCourseSerializer,
+)
+
+
+class MyEnrolledCourseView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        summary="내 수강 목록 조회",
+        tags=["Accounts"],
+        responses={
+            200: MyEnrolledCourseSerializer(many=True),
+            401: OpenApiExample("인증 에러", value={"error_detail": "자격 인증 데이터가 제공되지 않았습니다."}),
+        },
+    )
+    def get(self, request: Request) -> Response:
+        user = cast(User, request.user)
+        enrollments = user.enrollmentrequest_set.all().select_related("cohort", "cohort__course")
+
+        data = []
+        for en in enrollments:
+            data.append({"cohort": en.cohort, "course": en.cohort.course})
+
+        serializer = MyEnrolledCourseSerializer(data, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)

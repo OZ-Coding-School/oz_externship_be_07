2af503e066e8aad122c6c4564f1fd41e7ff583d3
unkn0wn-j <wjsrnr470@gmail.com>
2026-03-25 12:58:43 +0900

    ♻️ refactor : 중복 코드 수정 후 필요 없는 test코드 삭제

--- apps/subject/views/cohort_permissions.py
+++ apps/subject/views/cohort_permissions.py
@@ -1,7 +1,9 @@
 from apps.core.permissions import IsStaffUser
 
+
 class IsSubjectStaffUser(IsStaffUser):
     message = "권한이 없습니다."
+
 
 class CanViewCohortList(IsSubjectStaffUser):
     message = "이 리소스를 조회할 권한이 없습니다."

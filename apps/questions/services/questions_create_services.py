6eca743b0c60607f9ef86cf7345d1639d75db065
sangbo <tkdqh57@naver.com>
2026-03-26 11:10:11 +0900

    ♻️ refactor : 질문 등록 권한에 어드민 추가

--- apps/questions/services/questions_create_services.py
+++ apps/questions/services/questions_create_services.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.exceptions import ValidationError
 
 from apps.questions.models import QuestionCategories, QuestionImages, Questions
+from apps.users.choices import UserRole
 from apps.users.models.models import User
 
 
@@ -18,8 +19,12 @@ class QuestionCreateService:
         image_url_list: list[str] | None = None,
     ) -> Questions:
 
-        if user.role != "STUDENT":
-            raise PermissionDenied("질문 등록은 수강생 권한이 필요합니다.")
+        # 수강생과 어드민 권한
+        allowed_roles = [UserRole.STUDENT, UserRole.ADMIN]
+
+        # 수강생 이상 권한은 조회 가능
+        if user.role not in allowed_roles:
+            raise PermissionDenied("질문 등록 권한이 없습니다.")
 
         category = QuestionCategories.objects.get(id=category_id)
 

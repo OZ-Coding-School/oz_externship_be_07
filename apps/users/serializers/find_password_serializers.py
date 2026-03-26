d0448de4be8b76f458c03627fb7b73a28a4961c8
SUNO-Back <tnsgusdh1234@gmail.com>
2026-03-26 18:03:19 +0900

    ✨ feat     : 비밀번호 재설정에 검증추가

--- apps/users/serializers/find_password_serializers.py
+++ apps/users/serializers/find_password_serializers.py
@@ -1,8 +1,13 @@
 from typing import Any, Dict
 
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 
 class PasswordFindSerializer(serializers.Serializer[Dict[str, Any]]):
     email_token = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
+
+    def validate_new_password(self, value: str) -> str:
+        validate_password(value)
+        return value

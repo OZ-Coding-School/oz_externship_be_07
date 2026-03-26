13372962f68f4f12c6202e6b9cc14abb6ea46277
sangbo <tkdqh57@naver.com>
2026-03-26 15:23:35 +0900

    ♻️ refactor : API명세서 잘못 봐서 query_parameter없기에 삭제 및 로그인 권한 부분 수정

--- apps/questions/views/user_categories_list_views.py
+++ apps/questions/views/user_categories_list_views.py
@@ -1,7 +1,9 @@
-from drf_spectacular.utils import OpenApiParameter, extend_schema
+from typing import cast
+
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,31 +13,21 @@ from apps.questions.services.user_categories_list_services import (
     QuestionCategoryService,
 )
 from apps.users.choices import UserRole
+from apps.users.models.models import User
 
 
 # API명세서 102번 유저 카테고리 조회
 class UserCategoryListView(APIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
     @extend_schema(
         tags=["user_category"],
         summary="유저 카테고리 목록 조회",
         description="수강생 권한을 가진 유저가 카테고리 목록을 조회합니다.",
-        parameters=[
-            OpenApiParameter(name="page", description="페이지 번호", required=False, type=int, default=1),
-            OpenApiParameter(name="size", description="페이지당 항목 수", required=False, type=int, default=10),
-            OpenApiParameter(name="search_keyword", description="검색어", required=False, type=str),
-            OpenApiParameter(
-                name="category_type", description="카테고리 타입(small, medium, large)", required=False, type=str
-            ),
-        ],
         responses={200: UserCategorySerializer(many=True)},
     )
     def get(self, request: Request) -> Response:
-        if not request.user or not request.user.is_authenticated:
-            return Response({"error_detail": "로그인이 필요합니다."}, status=status.HTTP_401_UNAUTHORIZED)
-
-        user = request.user
+        user = cast(User, request.user)
 
         # 수강생 이상의 권한
         allowed_roles = [UserRole.STUDENT, UserRole.TA, UserRole.OM, UserRole.LC, UserRole.ADMIN]

8ca58e8c00ff78f0b3976d33b6ffe05f4273276e
pcw8233 <pcw3511@gmail.com>
2026-03-26 18:00:27 +0900

    🐛 fix      : test code refix git rebase 최신화

--- apps/questions/tests/admin/test_admin_categories.py
+++ apps/questions/tests/admin/test_admin_categories.py
@@ -4,20 +4,37 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.test import APIClient
 
 from apps.questions.models import QuestionCategories
+from apps.users.models.models import User
 
 
 class AdminQuestionListTests(TestCase):
     category: QuestionCategories
     url: str
+    admin_user: User
 
     @classmethod
     def setUpTestData(cls) -> None:
         cls.category = QuestionCategories.objects.create(name="테스트 카테고리")
-        cls.url = reverse("admin_questions:admin_question_list")
+        cls.url = reverse("admin_questions:admin-question-list")
+        cls.admin_user = User.objects.create_superuser(
+            email="admin@test.com",
+            password="admin123",
+            name="testadmin",
+            nickname="testadmin",
+            phone_number="010-1111-2222",
+            gender="MALE",
+            birthday="1970-01-01",
+            role="ADMIN",
+            is_staff=True,
+            is_superuser=True,
+        )
 
     def test_get_question_list_full_coverage(self) -> None:
+        client = APIClient()
+        client.force_authenticate(user=self.admin_user)
         params: Mapping[str, Any] = {
             "page": 1,
             "size": 10,
@@ -27,7 +44,7 @@ class AdminQuestionListTests(TestCase):
             "sort": "oldest",
         }
 
-        response = cast(Response, self.client.get(self.url, data=params, format="json"))
+        response = cast(Response, client.get(self.url, data=params, format="json"))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 

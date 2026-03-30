9a55b3065570a57b1cdb84eed9adfed9006f617e
whdgk7176git config --global user.name whdgk7176 <whdgk7176@gmail.com>
2026-03-30 10:41:51 +0900

    ✨ feat     : question타입별 에러지정
    # Conflicts:
    #	apps/exam/serializers/exam_question_serializers.py
    #	apps/exam/services/exam_question_services.py

--- apps/exam/tests/test_exam_question.py
+++ apps/exam/tests/test_exam_question.py
@@ -142,7 +142,7 @@ class ExamQuestionAPITest(APITestCase):
         response = self.client.post(url, self.question_data, format="json")
         self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
         self.assertEqual(
-            response.data["error_detail"], "해당 쪽지시험에 등록 가능한 문제 수 또는 총 배점을 초과했습니다,"
+            response.data["error_detail"], "해당 쪽지시험에 등록 가능한 문제 수 또는 총 배점을 초과했습니다."
         )
 
     def test_update_question_success(self) -> None:

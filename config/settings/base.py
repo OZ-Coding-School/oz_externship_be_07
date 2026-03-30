ac2387d8e25a2302f42ac3b99a380637abf155d6
2joonkim <joon271694@gmail.com>
2026-03-30 13:42:31 +0900

    🐛 fix      : bearer 세팅 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -238,6 +238,15 @@ SPECTACULAR_SETTINGS = {
         "filter": True,  # True 이면 Swagger UI에서 'Filter by Tag' 검색이 가능합니다
     },
     "SERVE_PERMISSIONS": ["rest_framework.permissions.AllowAny"],
+    "SECURITY": [
+        {
+            "BearerAuth": {
+                "type": "http",
+                "scheme": "bearer",
+                "bearerFormat": "JWT",
+            }
+        }
+    ],
 }
 
 # SMTP Settings
